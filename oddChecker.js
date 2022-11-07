e6ef43d6c05fa1f10dfac27bed2c9a30af249ef9
Abdul <ffred7760@gmail.com>
2022-11-07 11:41:30 -0500

    Create a Fxn to check if a # is Odd or Not

---
+++ oddChecker.js
@@ -0,0 +1,9 @@
+// Create a function to determine if a number is Odd or NOT:
+
+
+const isOdd= function (num) {
+  return num % 2 !== 0;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("6 is odd: " + isOdd(6));
