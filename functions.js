f9a541968a572abe88930c567adb98afe60a3b3e
Abdul <ffred7760@gmail.com>
2022-11-07 11:10:11 -0500

    Using function with examples

---
+++ functions.js
@@ -0,0 +1,23 @@
+/*
+const sayHello = function () {
+  console.log("Hello World!")
+}
+// This is to invoke the function
+sayHello();
+
+
+
+// Instead of having to repeat our code, we can simply pass along their names to our function.
+const sayHello = function (name) {
+  console.log(`Hello, ${name}`)
+}
+
+sayHello('Afgunz');
+sayHello('Fenix');
+sayHello('ALL HATE')
+
+*/
+
+// Using function to determine whether a number is EVEN.
+
+
