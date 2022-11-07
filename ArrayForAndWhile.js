3c0cf35b521c7c2105b3468a27818aa3cf478e53
Abdul <ffred7760@gmail.com>
2022-11-07 17:50:04 -0500

    Using For and While Loops to list items in Array

---
+++ ArrayForAndWhile.js
@@ -0,0 +1,10 @@
+// going camping; need to store and organize all items:
+
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+//To Print the list:
